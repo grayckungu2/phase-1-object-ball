f069d8ab9a5017efa9a1faf4f577de54b2dba139
Grace kungu <grayckungu2@gmail.com>
2023-06-20 21:17:00 +0300

    assignment completed

---
+++ object - ball .js
@@ -0,0 +1,153 @@
+
+function gameobject(params) {
+    const obj = {
+    home: {
+        teamName: "Brooklyn Nets",
+        colors: ["Black", "White"],
+        players: {
+            "Alan Anderson":{
+                number: 0,
+                shoe: 16,
+                points: 22,
+                rebounds: 12,
+                assits: 12,
+                steals: 3,
+                blocks: 1,
+                slamDunks: 1,
+            
+            },
+            "Brook Lopez":{
+                number: 11,
+                shoe: 17,
+                points: 17,
+                rebounds: 19,
+                assits: 10,
+                steals: 3,
+                blocks: 1,
+                slamDunks: 16,
+
+            },
+            "Reggie Evans":{
+                number: 30,
+                shoe: 14,
+                points: 12,
+                rebounds: 12,
+                assits: 12,
+                steals: 12,
+                blocks: 12,
+                slamDunks: 7,
+            },
+            "Mason Plumlee":{
+                number: 1,
+                shoe: 19,
+                points: 26,
+                rebounds: 12,
+                assits: 6,
+                steals: 3,
+                blocks: 8,
+                slamDunks: 5,
+            },
+            "Jason Terry":{
+                number: 21,
+                shoe: 15,
+                points: 19,
+                rebounds: 2,
+                assits: 2,
+                steals: 4,
+                blocks: 11,
+                slamDunks: 1,
+            },
+
+            away: {
+                teamName: "Charlotte Hornets",
+                colors: ["Turquoise", "Purple"],
+                players: {
+                  "Jeff Adrien": {
+                    number: 4,
+                    shoe: 18,
+                    points: 10,
+                    rebounds: 1,
+                    assits: 1,
+                    steals: 2,
+                    blocks: 7,
+                    slamDunks: 2,
+                },
+                "DeSagna Diop": {
+                    number: 2,
+                    shoe: 14,
+                    points: 24,
+                    rebounds: 12,
+                    assits: 12,
+                    steals: 4,
+                    blocks: 5,
+                    slamDunks: 5,
+                  },
+                  "Bismack Biyombo": {
+                    number: 0,
+                    shoe: 16,
+                    points: 12,
+                    rebounds: 4,
+                    assits: 7,
+                    steals: 7,
+                    blocks: 15,
+                    slamDunks: 10,
+                  },
+                  "Ben Gordon": {
+                    number: 8,
+                    shoe: 15,
+                    points: 33,
+                    rebounds: 3,
+                    assits: 2,
+                    steals: 1,
+                    blocks: 1,
+                    slamDunks: 0,
+                  },
+                  "Brendan Hayword": {
+                    number: 33,
+                    shoe: 15,
+                    points: 6,
+                    rebounds: 12,
+                    assits: 12,
+                    steals: 22,
+                    blocks: 5,
+                    slamDunks: 12,
+                  },
+                },
+              },
+             
+             
+ }      
+}
+    }
+}
+console.log(gameObject());
+/*
+{
+  home: {
+    teamName: "",
+    colors: [...],
+    players: {
+      "Alan Anderson": {...},
+      "Reggie Evans": {...}
+    }
+  },
+  away: {
+    ...
+  } 
+}
+*/
+function homeTeamName() {
+    let object = gameObject()
+    return object['home']['teamName']
+  }
+  
+  console.log(homeTeamName())
+  // logs "Brooklyn Nets"
+  function homeTeamName(){
+    return gameObject()['home']['teamName']
+  }
+  
+  console.log(homeTeamName())
+  // logs "Brooklyn Nets"
+  
+
